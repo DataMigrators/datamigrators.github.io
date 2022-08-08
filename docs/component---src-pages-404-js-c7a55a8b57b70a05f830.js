3ae71a311ea835a22dfba879d23663da2a5ece5d
Tim <tim.white@datamigrators.com>
2022-08-08 16:50:04 +1000

    added docs folder which contains the built website (hope this doesn't slow the repo to a crawl)

---
+++ docs/component---src-pages-404-js-c7a55a8b57b70a05f830.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexample=self.webpackChunkexample||[]).push([[8883],{429:function(e,n,t){t.r(n);var o=t(7294),r=t(9435),s=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return o.createElement(r.e9,{links:s})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c7a55a8b57b70a05f830.js.map
